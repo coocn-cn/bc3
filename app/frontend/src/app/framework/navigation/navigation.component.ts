643c91e9d3441a4600853d15b9833e3ca5f3605c
ssoor <icafe880@gmail.com>
2020-11-21 20:02:44 -0500

    feature: Switch frontend framework to angular

---
+++ app/frontend/src/app/framework/navigation/navigation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'framework-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.less']
+})
+export class NavigationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
